316bd7d33652c52493c0ea5aa1e23a88ce3e1af7
Mohamed Nabil <101708706+mnabil711@users.noreply.github.com>
2022-11-01 03:54:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import QuoteBox from "./QuoteBox";
 function App() {
-  const [mainColor, setMainColor] = useState("#16a085");
+  const [mainColor, setMainColor] = useState("");
 
   return (
     <div className="app" style={{ backgroundColor: mainColor }}>
       <div className="quote">
         <QuoteBox changeColor={(color) => setMainColor(color)} />
-        {/* <p>by mnabil711</p> */}
         <span>by </span>
         <a href="https://github.com/mnabil711" target="_blank" rel="noreferrer">
           mnabil711
